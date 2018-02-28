101adda196900f7be6bfceee05ddaf258a63973d
Szczebrzeszyn <krystian.p20@interia.pl>
2018-02-27 20:18:22 -1200

    praca domowa

--- python/cezar.py
+++ python/cezar.py
@@ -12,26 +12,30 @@ def szyfruj(tekst, klucz):
         ascii = ord(i) + klucz
         if ord(i) == 32:
             ascii = 32
-        if ascii > 90 and ascii < 97:
+        if ascii > 90 and ascii < 97 or ascii > 122:
             ascii -= 26
-        elif ascii > 122:
-            ascii -= 26
+        # elif ascii > 122:
+        #     ascii -= 26
 
         szyfrogram += chr(ascii)
 
     return szyfrogram
 
 
-def deszyfruj(szyfrogram, klucz):
+def deszyfruj(szyfrogram, klucz): # troche duzo ifow, ale narazie nwm jak to przyspieszyc i uproscic. wazne ze dziala
     tekst = ""
     for i in szyfrogram:
         ascii = ord(i) - klucz
+        # print(i, ascii)
         if ord(i) == 32:
             ascii = 32
-        if ascii > 90 and ascii < 97:
+        if ascii < 65 and ascii != 32:
+            ascii += 26
+        if ascii > 90 and ascii < 97 and ascii != 32:
+            ascii += 26
+        if i.isdigit():
             ascii -= 26
-        elif ascii > 122:
-            ascii -= 26
+        # print(i, ascii)
         tekst += chr(ascii)
 
     return tekst
@@ -44,7 +48,8 @@ def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
     szyfrogram = szyfruj(tekst, klucz)
-
+    # szyfrogram = "VCBIU FHCDUD MHVW SURVWB"
+    # klucz = 3
     print(szyfrogram)
     print(deszyfruj(szyfrogram, klucz))
 
@@ -53,4 +58,4 @@ def main(args):
 
 if __name__ == '__main__':
     import sys
-sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
